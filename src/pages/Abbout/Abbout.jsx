7416546bbac2a168319a45c722c6e674f0621c3b
Luis071 <82769401+Luis071@users.noreply.github.com>
2022-08-21 19:54:04 -0400

    about

--- src/pages/Abbout/Abbout.jsx
+++ src/pages/Abbout/Abbout.jsx
@@ -21,7 +21,6 @@ const Abbout = () =>
             </center>
             <br/>
             <br/>
-
           <center>
             <div class="tarjetas">
               <div class="container">
@@ -58,9 +57,40 @@ const Abbout = () =>
                   </div>
                 </div>
               </div>
-
             </div>
           </center>
+                <br/>
+                <br/>
+                <br/>
+                    <center>
+                    <t2>Propietarios</t2>
+                    <br/>
+                    <br/>
+              <div class="tarjetas">
+              <div class="container">
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="card">
+                        <div className="img1"><img src="https://media.discordapp.net/attachments/985659106796929095/1011034976600469676/unknown.png" alt="" /></div>
+                        <div className="main-text">
+                          <h2>MISIÓN</h2>
+                          <p>Satisfacer a nuestros clientes brindando un servicio de calidad, capaz con cde lograr empleados proactivos, mostrando compromiso con nuestros clientes y proveedores.</p>
+                        </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card">
+                        <div className="img1"><img src="https://media.discordapp.net/attachments/985659106796929095/1011035107420798986/unknown.png" alt="" /></div>
+                        <div className="main-text">
+                          <h2>VISIÓN</h2>
+                          <p>Proporcionar hogares para familias de todos los niveles de ingresos en nuestro país mediante la construcción de viviendas que mejoren la vida. calidad de vida de los usuarios. </p>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </center>
         </body>
     </div>
 
