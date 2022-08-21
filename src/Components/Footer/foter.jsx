b69ff87fa6695f64e42b0203ea2358a9a9bc46a9
Christopher Richardson <49372253+cy3e@users.noreply.github.com>
2022-08-21 19:53:35 -0400

    proyects

--- src/Components/Footer/foter.jsx
+++ src/Components/Footer/foter.jsx
@@ -8,8 +8,8 @@ const foter=() =>
             <div class="container-fluid text-center text-white bg-dark p-3">
                 <p class="small" className='bio'>© 2022 Soto Montero - Construcciones y Servicios - Todos los derechos reservados.</p>
                 <div className='Social'>
-                <a href='https://www.facebook.com/Soto-Montero-construcciones-y-servicios-103003851915449'><img src='../../icons/facebook.png' alt='Facebook'/></a>
-                <a href='https://www.facebook.com/Soto-Montero-construcciones-y-servicios-103003851915449'><img src='../../icons/Gmail.png' alt='Gmail'/></a>
+                <a href='https://www.facebook.com/Soto-Montero-construcciones-y-servicios-103003851915449'><img className='fb' src='../../icons/facebook.png' alt='Facebook'/></a>
+                <a href='https://www.facebook.com/Soto-Montero-construcciones-y-servicios-103003851915449'><img className='gm' src='../../icons/Gmail.png' alt='Gmail'/></a>
                 <a >809-782-6590</a>
                 <a>809-904-5587</a>
                 </div>
