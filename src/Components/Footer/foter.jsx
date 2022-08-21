31fa2003eb28bd0c3b4faf992c87125bc8d89d17
Christopher Richardson <49372253+cy3e@users.noreply.github.com>
2022-08-21 17:45:47 -0400

    footer inicio

--- src/Components/Footer/foter.jsx
+++ src/Components/Footer/foter.jsx
@@ -6,6 +6,12 @@ const foter=() =>
         <div className='App'>
             <div class="container-fluid text-center text-white bg-dark p-3">
                 <p class="small">© 2022 Soto Montero - Construcciones y Servicios - Todos los derechos reservados.</p>
+                <div className='Social'>
+                <a href='https://www.facebook.com/Soto-Montero-construcciones-y-servicios-103003851915449'><img src='' alt='Facebook'/></a>
+                <a href='https://www.facebook.com/Soto-Montero-construcciones-y-servicios-103003851915449'><img src='' alt='Gmail'/></a>
+                <a>809-782-6590</a>
+                <a>809-904-5587</a>
+                </div>
             </div>
         </div>
 
