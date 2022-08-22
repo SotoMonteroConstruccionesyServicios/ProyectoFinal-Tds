525af394b685ba3e6b0fc0f826d359f7404f330b
Christopher Richardson <49372253+cy3e@users.noreply.github.com>
2022-08-21 23:26:18 -0400

    service 2

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,7 @@
 //imports the react resources
 import React,{Suspense } from 'react';
 import { Canvas } from "@react-three/fiber";
-import Casa from "../../Components/casa/casa";
+import Casa from "../../Components/casa/Casa";
 import { Center, OrbitControls } from '@react-three/drei';
 
 import './Services.css';
@@ -9,16 +9,14 @@ import './Services.css';
 function  Services() {
   return (
     <>
-    
     <div className='d3'>
-      <Canvas camera={{zoom: 1, position:[15,20,15]}}>
+    <Canvas camera={{zoom: 1, position:[15,20,15]}}>
       <ambientLight intensity={0.5}/>
       <pointLight position={[35,35,0]} intensity={0.4}/>
       <pointLight position={[-35,35,0]} intensity={0.4}/>
         <Casa/>
       <OrbitControls/>
     </Canvas>
-    
     </div>
   </>
   );
