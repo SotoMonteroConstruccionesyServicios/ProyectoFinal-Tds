8e394557b359962f6e95798f25f9d2d3e045cbe4
Luis071 <82769401+Luis071@users.noreply.github.com>
2022-08-21 21:39:08 -0400

    about

--- src/pages/Abbout/Abbout.jsx
+++ src/pages/Abbout/Abbout.jsx
@@ -61,7 +61,6 @@ const Abbout = () =>
           </center>
                 <br/>
                 <br/>
-                <br/>
                     <center>
                     <t2>Propietarios</t2>
                     <br/>
@@ -71,28 +70,34 @@ const Abbout = () =>
                 <div className="row">
                   <div className="col-md-4">
                     <div className="card">
-                        <div className="img1"><img src="https://media.discordapp.net/attachments/985659106796929095/1011034976600469676/unknown.png" alt="" /></div>
-                        <div className="main-text">
-                          <h2>MISIÓN</h2>
-                          <p>Satisfacer a nuestros clientes brindando un servicio de calidad, capaz con cde lograr empleados proactivos, mostrando compromiso con nuestros clientes y proveedores.</p>
+                        <div className="img3"><img src="https://media.discordapp.net/attachments/985659106796929095/1011082509381030000/Fondo.jpg" alt="" /></div>
+                        <div className="img2"><img src="https://media.discordapp.net/attachments/985659106796929095/1011083568690245832/unknown.png" alt="" /></div>
+                        <div className="main-text1">
+                          <h3>Felipe A. Soto Carmona</h3>
+                          <p>Ingeniero Civil/Arquitecto en Jefe.</p>
+                          <p>Propietario princiapal de la compañia Soto Montero - Construcciones y Servios</p>
                         </div>
                     </div>
                   </div>
                   <div className="col-md-4">
                     <div className="card">
-                        <div className="img1"><img src="https://media.discordapp.net/attachments/985659106796929095/1011035107420798986/unknown.png" alt="" /></div>
-                        <div className="main-text">
-                          <h2>VISIÓN</h2>
-                          <p>Proporcionar hogares para familias de todos los niveles de ingresos en nuestro país mediante la construcción de viviendas que mejoren la vida. calidad de vida de los usuarios. </p>
+                        <div className="img3"><img src="https://media.discordapp.net/attachments/985659106796929095/1011082509381030000/Fondo.jpg" alt="" /></div>
+                        <div className="img2"><img src="https://media.discordapp.net/attachments/985659106796929095/1011083649682260098/unknown.png?width=380&height=448" alt="" /></div>
+                        <div className="main-text1">
+                          <h3>Elena Montero</h3>
+                          <p>Lic. en Marketing/Lic. en Bienesraices</p>
+                          <p>Co-Propietaria de la compañia Soto Montero - Construcciones y Servios</p>
                         </div>
                     </div>
                   </div>
                   <div className="col-md-4">
                     <div className="card">
-                        <div className="img1"><img src="https://media.discordapp.net/attachments/985659106796929095/1011035107420798986/unknown.png" alt="" /></div>
-                        <div className="main-text">
-                          <h2>VISIÓN</h2>
-                          <p>Proporcionar hogares para familias de todos los niveles de ingresos en nuestro país mediante la construcción de viviendas que mejoren la vida. calidad de vida de los usuarios. </p>
+                        <div className="img3"><img src="https://media.discordapp.net/attachments/985659106796929095/1011082509381030000/Fondo.jpg" alt="" /></div>
+                        <div className="img2"><img src="https://media.discordapp.net/attachments/985659106796929095/1011083736097505350/unknown.png" alt="" /></div>
+                        <div className="main-text1">
+                          <h3>Luis Soto</h3>
+                          <p>Lic.en Software/Gerente de Servicios</p>
+                          <p>Administra el servicio al cliente de la compañia Soto Montero - Construcciones y Servios</p>
                         </div>
                     </div>
                   </div>
