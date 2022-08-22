8b7562f21e4d5c1f275f704daa23fc6229852602
Christopher Richardson <49372253+cy3e@users.noreply.github.com>
2022-08-22 16:58:19 -0400

    carrousel se fue

--- src/Components/Carrousel/Carrousel.jsx
+++ src/Components/Carrousel/Carrousel.jsx
@@ -13,7 +13,7 @@ export function Carrousel(){
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://picsum.photos/1080/500"
+            src="https://media.discordapp.net/attachments/985659106796929095/1010674833576710144/unknown.png?width=720&height=283"
             alt="First slide"
           />
           <Carousel.Caption>
@@ -24,7 +24,7 @@ export function Carrousel(){
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://picsum.photos/1080/500"
+            src="https://media.discordapp.net/attachments/985659106796929095/1010675128813764698/unknown.png"
             alt="Second slide"
           />
   
@@ -36,7 +36,7 @@ export function Carrousel(){
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://picsum.photos/1080/500"
+            src="https://media.discordapp.net/attachments/985659106796929095/1010675218534117376/unknown.png?width=720&height=405"
             alt="Third slide"
           />
   
